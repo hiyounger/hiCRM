a61477a6b9d0540dc54723066d9b801e0e817dd6
Yao Dong <2870055191@qq.com>
2019-07-30 19:34:35 +0800

    one

--- src/main/java/com/hiyoung/manage/bussiness/service/BuinessIf.java
+++ src/main/java/com/hiyoung/manage/bussiness/service/BuinessIf.java
@@ -13,10 +13,12 @@ import com.hiyoung.manage.bussiness.entity.Buiness;
 
 public interface BuinessIf {
 int getcount();
-
+int getCount2(String name);
 List<Buiness> getbyPage(int page,int rows);
 List<Buiness> getbyPagetheone(int page,int rows,int theone);
 List<Buiness> getbyPageAnd(int page,int rows,String name);
 int insert(Buiness record);
 int deletebyid(Integer id);
+List<Buiness> newget(int page,int rows,Buiness buiness);
+
 }
